b3fee4f7ad867dc9333ab4acb2527b067cb93b59
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-01-07 09:53:20 -0300

    Directives and parant component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName:string = "Dante";
   title = 'angular-node-webApp';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FristComponentComponent } from './components/frist-component/frist-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FristComponentComponent
+    FristComponentComponent,
+    ParentDataComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule
