2ed77c4aae3d35935ccbb4c44835bdfa8d5a3a81
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-01-07 13:52:03 -0300

    if render

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FristComponentComponent } from './components/frist-component/frist-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FristComponentComponent,
     ParentDataComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    IfRenderComponent,
   ],
   imports: [
     BrowserModule
